8384a6b3ccbf7d3da8152ab5ea4a371eb67051b2
julio vera <verajulio823@gmail.com>
2016-11-29 10:57:00 -0500

    implementation pdfcontroller

--- RepositoryInstitutional/src/com/unsa/controller/PdfBoxController.java
+++ RepositoryInstitutional/src/com/unsa/controller/PdfBoxController.java
@@ -1,5 +1,52 @@
 package com.unsa.controller;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccessFile;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
 public class PdfBoxController {
+	private PDFTextStripper pdfStripper = null;
+    private PDDocument pdDoc = null;
+    private COSDocument cosDoc = null;
+    private String path;
+    private String result;
+
+	
+	public PdfBoxController(String path) {
+		// TODO Auto-generated constructor stub
+		this.path=path;
+		this.result =getText();
+	}
+	
+	public String getResultText(){
+		return result;
+	}
+	
+	private String getText(){
+        File file = new File(path);
+        String parsedText ="";
+        try {
+        	PDFParser parser = new PDFParser(new RandomAccessFile(file,"r")); 
+            parser.parse();
+            cosDoc = parser.getDocument();
+            pdfStripper = new PDFTextStripper();
+            pdDoc = new PDDocument(cosDoc);
+            pdfStripper.setStartPage(1);
+            pdfStripper.setEndPage(5);
+            parsedText = pdfStripper.getText(pdDoc);
+           // System.out.println(parsedText);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } 
+
+		return parsedText;
+	}
+	
 
 }
--- RepositoryInstitutional/src/com/unsa/src/Main.java
+++ RepositoryInstitutional/src/com/unsa/src/Main.java
@@ -1,11 +1,30 @@
 package com.unsa.src;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccessFile;
+import org.apache.pdfbox.io.RandomAccessRead;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import com.unsa.controller.PdfBoxController;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String path = "/home/pc-vera/Documentos/OCRUNSA/ocr-bci/B2-C-1660-1.pdf";
 		System.out.println("Bale Berga la Bida");
-
+		
+		PdfBoxController pdf = new PdfBoxController(path);
+		System.out.println(pdf.getResultText());
+		
+		
 	}
 
 }
